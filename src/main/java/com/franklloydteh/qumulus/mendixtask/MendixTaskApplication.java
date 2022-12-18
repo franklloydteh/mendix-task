c1ae44c70407f5bb0ee79bc8ba06f01f65158c86
eastwestageas <frank@sscaageas.com>
2022-12-19 07:39:31 +0800

    Initial Project from Spring Initializr

---
+++ src/main/java/com/franklloydteh/qumulus/mendixtask/MendixTaskApplication.java
@@ -0,0 +1,13 @@
+package com.franklloydteh.qumulus.mendixtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MendixTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MendixTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franklloydteh/qumulus/mendixtask/MendixTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklloydteh.qumulus.mendixtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MendixTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
